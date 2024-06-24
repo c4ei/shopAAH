9b7a638d2d29aa88d84a2101e5ea45af9a78aa18
c4ei <c4ei.net@gmail.com>
2024-06-24 23:51:51 +0900

    bottom

--- frontend/src/Layouts/Footer.js
+++ frontend/src/Layouts/Footer.js
@@ -78,15 +78,22 @@ export default function Footer() {
           <div className="row">
             <div className="col-lg-6">
               <p className="small text-muted mb-0">
-                &copy; 2024 All rights reserved.
+회사명 쇼핑뱅크 주소 인천 부평구 체육관로 18 5층 6050호 대경프라자<br/>
+사업자 등록번호 376-26-00310 대표 이규희 전화 010-6444-0111 팩스<br/>
+통신판매업신고번호 제2023인천부평1587호 개인정보 보호책임자<br/>
+씨포이아이(C4EI) 건강기능식품 영업신고서 제2019-0060180호 <br/>
               </p>
+              <b>C4EI 는 쇼핑뱅크의 파트너 사로 AAH 결재/지불을 대행 합니다.</b>
             </div>
             <div className="col-lg-6 text-lg-right">
               <p className="small text-muted mb-0">
-                AAH coin buy{" "}
                 <a className="text-white reset-anchor" href="https://c4ex.net" target="_blank">
-                  c4ex.net
-                </a>
+                  c4ex.net</a> - AAH coin 구매 
+                <br/>
+                <a className="text-white reset-anchor" href="https://c4ei.net" target="_blank">
+                  c4ei.net</a> - C4EI, AAH Coin 정보
+                <br/><br/>
+                &copy; 2024 All rights reserved.
               </p>
             </div>
           </div>
